5e3e959afe83623a92700ce30df7191eb8e85b48
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-22 23:22:50 +0900

    Add files via upload

---
+++ src/test/java/dev/oormthon/jejucart/JejucartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oormthon.jejucart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejucartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
