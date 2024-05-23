944b7f04e9c825b8c55282d527ed32de62ba155c
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-23 22:11:58 +0900

    fix: update webconfig

--- src/main/java/dev/goormthon/jejucart/global/config/WebConfig.java
+++ src/main/java/dev/goormthon/jejucart/global/config/WebConfig.java
@@ -1,9 +1,12 @@
 package dev.goormthon.jejucart.global.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
