2c2fb6737c0a2e72e3fae7b2485c6310ccce06dd
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-24 11:50:04 +0900

    fix: cors 설정

--- src/main/java/dev/goormthon/jejucart/global/config/WebConfig.java
+++ src/main/java/dev/goormthon/jejucart/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://jejucart-front.vercel.app")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true);
     }
