c9657baf9e84fb0b02ce317bd182098937444771
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-30 14:31:31 +0900

    add: EnableJpaAuditing 옵션 추가

--- src/main/java/dev/goormthon/ppoori/PpooriApplication.java
+++ src/main/java/dev/goormthon/ppoori/PpooriApplication.java
@@ -2,8 +2,10 @@ package dev.goormthon.ppoori;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PpooriApplication {
 
 	public static void main(String[] args) {
